733cd53909a9612536db8c3864ea2468be4d3063
devops <anjuri.kelaiya@birlasoft.com>
2020-04-14 12:39:23 +0530

    commit to test rebase

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,7 @@ public class Application {
     public static void main(String[] args) {
         int myInt = 7;
         String text = "Hello";
-        String name = "Anjuri";
+        String name = "Anjuri Kelaiya";
         String greeting = text + name;
         System.out.println(greeting);
         System.out.println("My number is:" + myInt);
