23a8afa5128dbb6602ed42a62b2dceff0f95617b
devops <anjuri.kelaiya@birlasoft.com>
2020-04-14 13:12:58 +0530

    for merge test

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     public static void main(String[] args) {
         int myInt = 7;
-        String text = "Hello";
+        String text = "Hello good noon";
         String name = "Anjuri Kelaiya";
         String greeting = text + name;
         System.out.println(greeting);
